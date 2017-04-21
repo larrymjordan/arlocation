94ed6a34335a2a43a93c8cc0ca6ca9b789a6d8f9
Larry M. Jordan <larrymoralesjordan@gmail.com>
2017-04-21 17:58:55 -0500

    Integrate threejs sample with angular-cli app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'app works!';
+  scene: THREE.Scene;
+  renderer: THREE.WebGLRenderer;
+  camera: THREE.PerspectiveCamera;
+  cube: THREE.Mesh;
+
+  ngOnInit() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild(this.renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+    this.cube = new THREE.Mesh( geometry, material );
+    this.scene.add( this.cube );
+
+    this.camera.position.z = 5;
+    this.render();
+  }
+
+  render() {
+	  requestAnimationFrame( ()=>{this.render()} );
+    this.cube.rotation.x += 0.1;
+    this.cube.rotation.y += 0.1;
+	  this.renderer.render(this.scene, this.camera );
+  }
 }
